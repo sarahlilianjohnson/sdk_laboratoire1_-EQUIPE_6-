80c4b639feb66aee91df5caa5f74c3739b83afc6
Sarah Johnson <sarahlilianjohnson@gmail.com>
2022-07-23 13:07:55 -0400

    Update to code

--- LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
+++ LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
@@ -1,7 +1,41 @@
 ﻿namespace LibrairieConversionNote
 {
-    public class ConversionAlphabetiqueToNote
+    public static class ConversionAlphabetiqueToNote
     {
-
+        public static string Transform(string note)
+        {   
+            if (note == "A")
+            {
+                return "LA";
+            }
+            else if (note == "B")
+            {
+                return "SI";
+            }
+            else if (note == "C")
+            {
+                return "DO";
+            }
+            else if (note == "D")
+            {
+                return "RE";
+            }
+            else if (note == "E")
+            {
+                return "MI";
+            }
+            else if (note == "F")
+            {
+                return "FA";
+            }
+            else if (note == "G")
+            {
+                return "SOL";
+            }
+            else
+            {
+                return "ERREUR";
+            }
+        }
     }
 }
---
+++ SDK_Laboratoire_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using LibrairieConversionNote;
+
+//On roule l'application non-stop, tant que CTRL+C
+while (true)
+{
+    Console.Write("Type de conversion à faire : ");
+    var input = Console.ReadLine();
+
+    //Si l'utilisateur tape note
+    if (input?.ToUpperInvariant() == "NOTE")
+    {
+        //On demande en boucle la conversion 
+        while (true)
+        {
+            Console.Write("Conversion à faire : ");
+            var note = Console.ReadLine();
+            string noteRetour = LibrairieConversionNote.ConversionAlphabetiqueToNote.Transform(note.ToUpper());//APPEL DE VOTRE LIB
+            Console.Write(noteRetour);
+            Console.WriteLine();
+        }
+    }
+}
